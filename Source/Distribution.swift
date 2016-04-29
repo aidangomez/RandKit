2870d179f55b001522a4cc04be4a382f1685b51a
Aidan Gomez <agomez@venturemedia.com>
2016-04-29 15:37:48 -0700

    Add XCode Project

---
+++ Source/Distribution.swift
@@ -0,0 +1,9 @@
+//
+//  Distribution.swift
+//  RandKit
+//
+//  Created by Aidan Gomez on 2016-04-29.
+//  Copyright © 2016 Aidan Gomez. All rights reserved.
+//
+
+import Foundation
---
+++ Source/RandKit.h
@@ -0,0 +1,14 @@
+//  Copyright © 2016 Aidan Gomez.
+//
+// This file is part of RandKit. The full RandKit copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RandKit.
+FOUNDATION_EXPORT double RandKitVersionNumber;
+
+//! Project version string for RandKit.
+FOUNDATION_EXPORT const unsigned char RandKitVersionString[];
