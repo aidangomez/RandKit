a546061b37ff3c420f5783696f747a6195832f7f
Aidan Gomez <agomez@venturemedia.com>
2016-05-02 12:29:55 -0700

    Add Random Sampling

--- Source/RandomGenerator.swift
+++ Source/RandomGenerator.swift
@@ -16,3 +16,39 @@ public class RandomGenerator<Element, D: DistributionType where D.Element == Ele
         return distribution.random()
     }
 }
+
+public class SamplingGenerator<Element, D: DiscreteDistributionType where D.Element == DiscreteValue>: GeneratorType {
+    let distribution: D
+    let elements: [Element]
+
+    public init(elements: [Element], distribution: D) {
+        self.elements = elements
+        self.distribution  = distribution
+    }
+
+    public func next() -> Element? {
+        let index = distribution.random()
+        return elements[index]
+    }
+
+    public func sample(count: Int = 1, withReplacement replacement: Bool = false) -> [Element] {
+        if replacement {
+            let indices = (0..<count).map({ _ in distribution.random() })
+            let values = indices.map({ elements[$0] })
+            return values
+        } else {
+            precondition(count <= elements.count)
+            var indices = [DiscreteValue]()
+            var values = [Element]()
+            for _ in 0..<count {
+                var index = distribution.random()
+                while indices.contains(index) {
+                    index = distribution.random()
+                }
+                indices.append(index)
+                values.append(elements[index])
+            }
+            return values
+        }
+    }
+}
--- Source/RandomValue.swift
+++ Source/RandomValue.swift
@@ -30,3 +30,42 @@ public func random(probability p: ContinuousValue) -> Bool {
     let v: ContinuousValue = random(0..<1)
     return v < p
 }
+
+/// Generate `sample` random values within range `inRange`
+public func random(sample count: Int, inRange range: Range<Int>, withReplacement replacement: Bool = false) -> [DiscreteValue] {
+    if replacement {
+        let values: [DiscreteValue] = (0..<count).map({ _ in uniform(range) })
+        return values
+    } else {
+        precondition(count <= range.count)
+        var values = [DiscreteValue]()
+        while values.count < count {
+            let value: DiscreteValue = uniform(range)
+            if values.contains(value) {
+                continue
+            } else {
+                values.append(value)
+            }
+        }
+        return values
+    }
+}
+
+public func random(sample count: Int, inRange range: Range<Int>, withReplacement replacement: Bool = false) -> [ContinuousValue] {
+    if replacement {
+        let values: [ContinuousValue] = (0..<count).map({ _ in uniform(range) })
+        return values
+    } else {
+        precondition(count <= range.count)
+        var values = [ContinuousValue]()
+        while values.count < count {
+            let value: ContinuousValue = uniform(range)
+            if values.contains(value) {
+                continue
+            } else {
+                values.append(value)
+            }
+        }
+        return values
+    }
+}
